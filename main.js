64fa84735534ad537d2b6d20ee6089126960a240
Julius Orn <julli@fjeldsted.is>
2015-01-10 15:54:52 +0000

    commit
    commit

--- main.js
+++ main.js
@@ -2,13 +2,19 @@ var arDrone = require('ar-drone');
 var client = arDrone.createClient();
 var http = require('http');
 var sockjs = require('sockjs');
-var movement_factor = 0.5;
+var movement_factor = 0.7;
 var echo = sockjs.createServer({ sockjs_url: 'http://cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js' });
+client.config('control:altitude_max', 3000);
+client.disableEmergency();
+
 echo.on('connection', function(conn) {
 	client.takeoff();
     conn.on('data', function(message) {
         var msg = JSON.parse(message);
 		switch(msg.command){
+			case "getVideo":
+					client.getPngStream()
+				break;
 			case "leftdown":
 				client.left(movement_factor);
 				break;
@@ -59,8 +65,16 @@ echo.on('connection', function(conn) {
 	});
 });
 
-var server = http.createServer();
+var server = http.createServer(function(req,res){
+    if (req	== "/png") {
+		var png = client.getPngStream();
+		res.writeHead(200, {'Content-Type': 'image/png' });
+		res.end(png,'binary');		
+	};
+
+});
 echo.installHandlers(server, {prefix:'/parrot'});
 server.listen(9999, '0.0.0.0');
 
 
+require('ar-drone-png-stream')(client, { port: 8000 });
